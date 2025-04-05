141e8ea7d42f04263687e4b7f5c7952c88ea31f3
ksonpethkar <ksonpethkar@gmail.com>
2025-04-06 00:37:46 +0530

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <h1>Welcome to the Accounting App</h1>;
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
